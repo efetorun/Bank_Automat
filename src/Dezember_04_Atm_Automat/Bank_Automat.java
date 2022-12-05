c7ef0cd2dde668d882202e208522bd378b9f7b6d
efe5561 <egzy5561@gmail.com>
2022-12-05 01:47:15 +0100

    Weiter mit Bankautomat

--- src/Dezember_04_Atm_Automat/Bank_Automat.java
+++ src/Dezember_04_Atm_Automat/Bank_Automat.java
@@ -25,6 +25,9 @@ public class Bank_Automat {
 
         if(benutzerPasswort.equals(passwort)&&benutzerKarteNummer.equals(benutzerKarteNummer)){
             menu();
+        }else {
+            System.out.println("Ihre Passwort ist nicht gültig !!");
+            eingabe();
         }
     }
 
@@ -49,12 +52,63 @@ public class Bank_Automat {
                     double betragAusZahlung= scan.nextDouble();
                     geldAuszahlung(betragAusZahlung);
                 }
-                case 4 : {}
-                case 5 : {}
-                case 6 : {}
+                case 4 : {
+                    System.out.println("Bitte geben Sie Ihre Iban Nummer : ");
+                    String iban=scan.nextLine();
+                    scan.nextLine();
+                    System.out.println("Bitte geben Sie den Überweisungsbetrag ein : ");
+                    double uberweisungsbetrag=scan.nextDouble();
+
+                    geldUberweisung(ibanKontrol(iban),uberweisungsbetrag);
+                }
+                case 5 : {
+                    passwortWechseln();
+                }
+                case 6 : {
+                    System.out.println("*********Tschüss!!***********");
+                    System.exit(0);
+                }
 
             }
 
+    }
+
+    private static void passwortWechseln() {
+        System.out.println("Bitte geben Sie Ihre Passwort ein : ");
+
+        String benutzerPasswort=scan.nextLine();
+
+        if (benutzerPasswort.equals(passwort)){
+            System.out.println("Bitte geben Sie Ihre neue Passwort ein : ");
+            passwort=scan.nextLine();
+            eingabe();
+        }else{
+            System.out.println("Bitte geben Sie Ihre gültige Passwort!!");
+            passwortWechseln();
+        }
+    }
+
+    private static void geldUberweisung(String iban, double uberweisungsbetrag) {
+   if (uberweisungsbetrag<=kontoGuthaben){
+       kontoGuthaben-=uberweisungsbetrag;
+       System.out.println(iban+"zu der Ibannummer "+ uberweisungsbetrag + "ist gesendet ");
+       kontoDetails();
+       menu();
+   }else {
+       System.out.println("Bitte geben Sie einen gültigen Überweisungsbetrag !!");
+   geldUberweisung(ibanKontrol(iban),scan.nextDouble());
+   }
+    }
+
+    private static String ibanKontrol(String iban) {
+    iban=iban.replaceAll("\\s","");
+    if (iban.startsWith("DE")&&iban.length()==26){
+
+    }else {
+        System.out.println("Bitte geben Sie eine gültige Ibannummer !!");
+        ibanKontrol(scan.nextLine());
+    }
+    return iban;
     }
 
     private static void geldAuszahlung(double betragAusZahlung) {
